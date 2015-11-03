7b579d5bfd10b1e378c2caf353041a9589e38e04
seandewar5 <seandewar5@googlemail.com>
2015-11-03 13:55:04 +0000

    more small progress

--- sd5nes/NESPPU.h
+++ sd5nes/NESPPU.h
@@ -9,12 +9,39 @@ struct NESPPURegisters
 {
 	union
 	{
-		u8 Control;
+		u8 control1;
 
-		u8 xScrollNt : 1;
-		u8 yScrollNt : 1;
+		u8 nameTableNum : 2;
 		u8 incAddr : 1;
-		// @TODO
+		u8 spritePatternTableNum : 1;
+		u8 bgPatternTableNum : 1;
+		u8 spritePixelSize : 1;
+		u8 control1Unused : 1;
+		u8 vBlankTriggerNMI : 1;
+	};
+
+	union
+	{
+		u8 control2;
+
+		u8 colorMode : 1;
+		u8 clipBg : 1;
+		u8 clipSprites : 1;
+		u8 displayBg : 1;
+		u8 displaySprites : 1;
+		u8 colorModeInfo : 3;
+	};
+
+	union
+	{
+		// Status register is read-only.
+		u8 status;
+
+		u8 statusUnused : 4;
+		u8 ignoreWrites : 1;
+		u8 scanlineSpritesCount : 1;
+		u8 sprite0Hit : 1;
+		u8 vBlankInProgress : 1;
 	};
 };
 
