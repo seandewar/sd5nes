6cc4d9d1688262f03e3cea7d9294016f68ebd4dd
seandewar5 <seandewar5@googlemail.com>
2016-01-26 14:48:15 +0000

    Progress on FM2 movie file support

--- sd5nes/NESTypes.h
+++ sd5nes/NESTypes.h
@@ -2,8 +2,12 @@
 
 #include <cstdint>
 
+/* Unsigned integer types */
 typedef std::uint8_t u8;
 typedef std::uint16_t u16;
 typedef std::uint32_t u32;
 
+/* Signed integer types */
 typedef std::int8_t s8;
+typedef std::int16_t s16;
+typedef std::int32_t s32;
